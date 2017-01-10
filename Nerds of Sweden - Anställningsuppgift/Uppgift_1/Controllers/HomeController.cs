b91f5536b13b9deb56e95e0c7cbe7b7f2c065078
runeanderberg <rune.anderberg@lund.ntig.se>
2017-01-10 11:07:12 +0100

    json things

---
+++ Nerds of Sweden - Anställningsuppgift/Uppgift_1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Uppgift_1.Models;
+
+namespace Uppgift_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
